ad762ef37b3c4ce204c5fa0917ccc95f3196f53b
Roshan Gautam <Roshan Gautam>
2016-11-21 19:38:08 -0600

    started coding: added print statements for user input

--- Personal_Information/src/Personal_Information.java
+++ Personal_Information/src/Personal_Information.java
@@ -1,16 +1,17 @@
-//A program that prompts an user to create an account with username and password and can be used for storing his private informations and notes.
-public class Personal_Information {
-/*outline
- 
-Creates a file to store the username and passwords.
-The program will consist of two options: create an account or login
-If create account option is selected, the username and password is stored in a file.
-The personal informations of the user is stored in a separate file.//Use java.io package to take input and to store information on file.
-Use Scanner to read user input and file input.
-Use a method to create an account
-Use a method to check the validity of password by setting some requirements for it.
-create method or code for User login and prompt the user to edit or view his information.
-code to logout
-
-*/
+import java.io.*;
+import java.util.Scanner;
+public class Personal_Information{
+	public static void main(String[]args) throws IOException{
+		try{
+		File accountInfo = new File("account.txt");
+		PrintWriter output = new PrintWriter(accountInfo);
+		System.out.println("Welcome to the InfoCabinet"+"\nType and Enter,\n(1)for creating an account.\n(2)for returning users login.\n(3)to exit.");
+		
+		}
+		
+catch (java.io.IOException ex){
+	System.out.println("I/O Errors: File is not found");
+		}
+	}
 }
+
