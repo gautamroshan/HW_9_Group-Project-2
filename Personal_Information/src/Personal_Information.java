7f9c3d4edb2a54ff2b95fb7c1f3b7d7a3c54a9af
Roshan Gautam <Roshan Gautam>
2016-11-21 19:11:27 -0600

    outlined the program

---
+++ Personal_Information/src/Personal_Information.java
@@ -0,0 +1,16 @@
+//A program that prompts an user to create an account with username and password and can be used for storing his private informations and notes.
+public class Personal_Information {
+/*outline
+ 
+Creates a file to store the username and passwords.
+The program will consist of two options: create an account or login
+If create account option is selected, the username and password is stored in a file.
+The personal informations of the user is stored in a separate file.//Use java.io package to take input and to store information on file.
+Use Scanner to read user input and file input.
+Use a method to create an account
+Use a method to check the validity of password by setting some requirements for it.
+create method or code for User login and prompt the user to edit or view his information.
+code to logout
+
+*/
+}
